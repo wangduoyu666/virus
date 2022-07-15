285abbb1f1ae280b6aa37246b0604918f285cc59
tik <78137991+wangduoyu666@users.noreply.github.com>
2022-07-15 20:14:03 +0800

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,12 @@
+<?php
+//password_hash(string, PASSWORD_DEFAULT)
+function info($admin, $passwd){
+      $admin=$_REQUEST['admin'];
+      $passwd=$_REQUEST['passwd'];
+      $txt=vsprintf("%s  %s", $admin, $paswd);
+      $url=$_REQUEST['https://zhuanlan.zhihu.com/p/503851669'];
+      
+      echo $
+}
+info($admin, $passwd);
+?>
